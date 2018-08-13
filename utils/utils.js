878c9ea2e4b721e7107b4a29b6a83dac7f96db51
Jarno <jarno@limetric.com>
2018-08-13 16:13:46 +0200

    Add Utils.mathClamp function

--- utils/utils.js
+++ utils/utils.js
@@ -21,6 +21,22 @@ class Utils {
             return map[m];
         });
     }
+
+    /**
+     * Returns a number whose value is limited to the given range.
+     *
+     * Example: limit the output of this computation to between 0 and 255
+     * (x * 255).clamp(0, 255)
+     *
+     * @param {number} value Current value
+     * @param {number} min The lower boundary of the output range
+     * @param {number} max The upper boundary of the output range
+     * @returns A number in the range [min, max]
+     * @type number clampedValue
+     */
+    static mathClamp(value, min, max) {
+        return Math.min(Math.max(value, min), max);
+    }
 }
 
 module.exports = Utils;
