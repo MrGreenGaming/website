0b3250fb14e650fae0eb96fd0a2d959d2fbb5dbf
Jarno <jarno@limetric.com>
2018-08-15 12:25:04 +0200

    Add app secret generation tool script

---
+++ tools/generateAppSecret.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcrypt');
+const saltWorkFactor = 10;
+
+const getRandomString = (charCount) => {
+    if (typeof(charCount) !== 'number')
+        charCount = 5;
+
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < charCount; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
+
+const writeData = (data) => {
+    const path = require('path');
+    const fileName = path.parse(__filename).name + '.txt';
+
+    const fs = require('fs');
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+
+        console.log('Data saved to ' + fileName);
+    });
+};
+
+const pass = (typeof(process.argv[2]) === 'string' ? process.argv[2] : getRandomString(30));
+
+bcrypt.genSalt(saltWorkFactor, (error, salt) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    bcrypt.hash(pass, salt, (error, hash) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+
+        console.log('Hash:', hash);
+
+        bcrypt.compare(pass, hash, (error, isMatch) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            console.log('Do they match?', isMatch);
+
+            if (isMatch)
+                writeData('Pass: ' + pass + '\nSalt: ' + salt + '\nHash: ' + hash);
+        });
+    });
+});
